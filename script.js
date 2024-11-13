869c0757a1a8ec6033ae775ff512279fe96fb035
FedericoD99 <526FDiPalma@frhsd.com>
2024-11-13 09:30:10 -0500

    started madlib almost done with javascript

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
-let userName =prompt('What is your name?')
-let userAge = prompt('How old are you')
+let objAdj = prompt('Name a Adjective')
+
+let objNoun = prompt('Give me a Animal')
+
+let storyName = prompt('Give me a name')
+
+let storyName1 =prompt('Give me a Name')
+
+let objNoun1 =prompt('Give me a item')
+
+let objPlace =prompt('Give me a place')
+
+let objNoun2=prompt('Give me a item')
+
+let objAdj1 =prompt('Give me a adjective')
+
+let storyName2 =prompt('Give me a name')
+
+let storyName3 =prompt('Give me a name')
+
+let objAdj =prompt('Give me a adjective')
+
+let objNoun2 =prompt('Give me a animal')
+
+let objAdj =prompt('Give me a adjective')
+
+let objNoun4 =prompt('Give me a animal')
+
+let objAdj =prompt('What is your name?')
+
+let objNoun =prompt('What is your name?')
+
+let objAdj =prompt('What is your name?')
+
+let actionVerb =prompt('What is your name?')
+
+let actionVerb =prompt('What is your name?')
+
+
 console.log (userName)
 console.log(userAge)
 
-let myStory = `Hello ${userName}, How are you doing today?  How does it feel to be ${userAge} years old?`
+let myStory = `One day, a (Adjective) [Animal] named [Name] decided to go on an adventure. [Name] had always dreamed of finding the legendary [Noun] hidden deep in the [Place].
+Armed with a [Noun] and a [Adjective] map, [Name] set off on the journey.Along the way, [Name] encountered a [Adjective] [Animal] who offered to help. 
+The two decided to travel together, hoping that the [Adjective] [Animal] would help them overcome the [Adjective] obstacles ahead. 
+They crossed a [Noun] and climbed a [Adjective] mountain, eventually reaching the entrance to the hidden cave.
+Inside, they found a [Adjective] treasure chest filled with [Plural Noun]. But to their surprise, it wasn’t gold or jewels—it was full of [Adjective] [Noun]!
+The two friends laughed and decided that the real treasure was the [Adjective] time they spent together. And so, they returned home, [Adjective] and ready for their next great adventure. `
 console.log(myStory)
 
 document.getElementById('story').innerHTML =myStory;
